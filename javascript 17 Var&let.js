c5ca87820a8f0912c2901336a25a8f26ff03e501
Abhishek kumar <89406337+abhishekrai17@users.noreply.github.com>
2023-05-25 14:00:01 +0530

    upto 19 uploaded

---
+++ javascript 17 Var&let.js
@@ -0,0 +1,18 @@
+//DIFFERENCE BETWEEN VAR AND LET
+
+
+let a = "abhishek"
+{
+    let a = "this is also a"
+    console.log(a);
+}
+console.log(a);
+
+
+
+var b = "thsi is a var"
+{
+    var b = "thsi is var inside braces"
+    console.log(b)
+}
+console.log(b)
