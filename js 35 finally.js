b508417fe3928e3f1b69d2d4f0be22c50cbd5b6a
Abhishek kumar <89406337+abhishekrai17@users.noreply.github.com>
2023-06-15 09:32:20 +0530

    uploaded 35

---
+++ js 35 finally.js
@@ -0,0 +1,14 @@
+//FINALLY
+
+try {
+    let a = 0
+    console.log(program)
+    console.log("program ran successsfully")
+}
+catch (err) {
+    console.log("this is an error")
+    console.log(p)
+}
+finally {
+    console.log("this is finally this will get executed if it is present no matter it catches an errror or  not")
+}
