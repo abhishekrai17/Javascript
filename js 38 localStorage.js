3d4d29b727a259f50ee7d44f77babbc7f9f3bb93
Abhishek kumar <89406337+abhishekrai17@users.noreply.github.com>
2023-06-19 10:08:20 +0530

    uploaded 38

---
+++ js 38 localStorage.js
@@ -0,0 +1,7 @@
+//TO OPEN LOCAL STORAGE INSPECT APPLICATION THEN LOCAL STORAGE
+localStorage.setItem("name", "harry")
+localStorage.getItem("name")
+localStorage.removeItem("name")
+localStorage.clear()
+localStorage.key(index)
+localStorage.length
